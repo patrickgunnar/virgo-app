526e6557dcbede1e196ddac8c371fd20b05325de
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-10 13:25:35 -0300

    refactor: improved the components

--- components/table/CredentialsTable.tsx
+++ components/table/CredentialsTable.tsx
@@ -4,6 +4,11 @@ import { useState } from "react";
 import Button from "../button/Button";
 
 
+interface ButtonType {
+    eventFn: () => void
+    label: string
+}
+
 enum STEPS {
     main = 0,
     login = 1,
@@ -14,40 +19,46 @@ const CredentialsTable = () => {
     // step state
     const [step, setStep] = useState<number>(0)
 
+    // button handler
+    const CurrentButton = ({eventFn, label}: ButtonType) => (
+        <Button className="flex justify-center items-center rounded-md font-bold text-base py-2 px-8
+        from-[#f1e499] via-[#b1ba27] to-[#888c08] bg-gradient-to-b drop-shadow-[0_1.4px_0.05rem] 
+        shadow-[#00000092] border-[#b1ba27] border-[1px] hover:opacity-75" onClick={eventFn}>
+            {label}
+        </Button>
+    )
+
     // current layout
     let currentLayout = (
         <>
             <div className="flex flex-col justify-center items-center h-16 w-[50%]">
-                <Button className="flex justify-center items-center p-2 rounded-md font-bold text-base
-                from-[#f1e499] via-[#b1ba27] to-[#888c08] bg-gradient-to-b drop-shadow-[0_1.4px_0.05rem] 
-                shadow-[#00000092] border-[#b1ba27] border-[1px] hover:opacity-75" onClick={() => setStep(1)}>
-                    Sign in
-                </Button>
+                <CurrentButton label="Sign in" eventFn={() => setStep(1)} />
             </div>
             <div className="flex flex-col justify-center items-center h-16 w-[50%]">
-                <Button className="flex justify-center items-center p-2 rounded-md font-bold text-base
-                from-[#f1e499] via-[#b1ba27] to-[#888c08] bg-gradient-to-b drop-shadow-[0_1.4px_0.05rem] 
-                shadow-[#00000092] border-[#b1ba27] border-[1px] hover:opacity-75" onClick={() => setStep(2)}>
-                    Sign up
-                </Button>
+                <CurrentButton label="Sign up" eventFn={() => setStep(2)} />
             </div>
         </>
     )
 
+    // back btn
+    const backButton = (
+        <div className="flex flex-col justify-start items-center p-4 h-fit w-fit self-start">
+            <CurrentButton label="Back" eventFn={() => setStep(0)} />
+        </div>
+    )
+
+    // if current step is login
     if(step === STEPS.login) currentLayout = (
-        <div>
-            <Button onClick={() => setStep(0)}>
-                Back
-            </Button>
+        <div className="flex flex-col justify-between items-center h-full w-full">
+            {backButton}
             Login 
         </div>
     )
 
+    // if current step is register
     if(step === STEPS.register) currentLayout = (
-        <div>
-            <Button onClick={() => setStep(0)}>
-                Back
-            </Button>
+        <div className="flex flex-col justify-between items-center h-full w-full">
+            {backButton}
             Register
         </div>
     )
