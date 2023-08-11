a7ad4218ec2ce558115d9fc6bfb5e0279e4efe4d
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-10 23:02:32 -0300

    feat: added logout handler

--- providers/SessionProvider.tsx
+++ providers/SessionProvider.tsx
@@ -4,11 +4,13 @@ import axios from "axios";
 import { createContext, useContext, useEffect, useState } from "react";
 import Cookies from 'js-cookie';
 import { UserType } from "@/types";
+import { useRouter } from "next/navigation";
 
 
 const SessionContext = createContext({
     session: null,
-    handleSession: (data: UserType | null) => {}
+    handleSession: (data: UserType | null) => {},
+    handleLogout: () => {}
 })
 
 interface SessionContextProviderProps {
@@ -16,6 +18,9 @@ interface SessionContextProviderProps {
 }
 
 export const SessionContextProvider: React.FC<SessionContextProviderProps> = ({ children }) => {
+    // get router
+    const router = useRouter()
+
     const [session, setSession] = useState<UserType | any>(null)
 
     // user token handler
@@ -42,10 +47,21 @@ export const SessionContextProvider: React.FC<SessionContextProviderProps> = ({ 
         setSession(data)
     }
 
+    // logout handler
+    const handleLogout = () => {
+        // remove cookies
+        Cookies.remove('tokenVirgo', { expires: 7, path: '/' })
+        // set user data
+        handleSession(null)
+        // refresh page
+        router.refresh()
+    }
+
     // context
     const context = {
         session: session,
-        handleSession: handleSession
+        handleSession: handleSession,
+        handleLogout: handleLogout
     }
 
     return (
