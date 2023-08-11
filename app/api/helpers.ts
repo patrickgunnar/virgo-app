64ed9bd8ffec94e556f183aa29fd02fb3bda988e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-10 22:22:17 -0300

    feat: exported the is valid email handler

--- app/api/helpers.ts
+++ app/api/helpers.ts
@@ -1,4 +1,5 @@
 // check if email is valid
-const isValidEmail = (email: string): boolean => {
-    return /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
+export const isValidEmail = (email: string): boolean => {
+    const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+    return regex.test(email)
 }
