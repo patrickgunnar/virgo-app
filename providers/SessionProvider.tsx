ecd086df88bb43697752189c97fbde6bedceab6c
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-10 21:14:03 -0300

    feat: session provider

---
+++ providers/SessionProvider.tsx
@@ -0,0 +1,58 @@
+'use client'
+
+import axios from "axios";
+import { createContext, useContext, useEffect, useState } from "react";
+import Cookies from 'js-cookie';
+import { UserType } from "@/types";
+
+
+const SessionContext = createContext({
+    session: null,
+    handleSession: (data: UserType | null) => {}
+})
+
+interface SessionContextProviderProps {
+    children: React.ReactNode
+}
+
+export const SessionContextProvider: React.FC<SessionContextProviderProps> = ({ children }) => {
+    const [session, setSession] = useState<any>(null)
+
+    // user token handler
+    const handleUserToken = () => {
+        // Set the token as an HttpOnly cookie
+        const sessionValue = Cookies.get('tokenVirgo')
+
+        if(sessionValue) {
+            axios.post('/api/decode-session', { sessionValue }).then((response) => {
+                // set data
+                setSession(response.data.data)
+            })
+        } else {
+            setSession(null)
+        }
+    }
+
+    useEffect(() => {
+        handleUserToken()
+    }, [])
+
+    // set session
+    const handleSession = (data: UserType | null) => {
+        setSession(data)
+    }
+
+    // context
+    const context = {
+        session: session,
+        handleSession: handleSession
+    }
+
+    return (
+        <SessionContext.Provider value={context}>
+            {children}
+        </SessionContext.Provider>
+    )
+}
+
+export const useSession = () => useContext(SessionContext)
