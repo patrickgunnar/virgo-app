e325e6bcb0a995bc27f1768bc54a145f0bf2f2f7
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-10 22:28:19 -0300

    feat: added special char handler

--- app/api/helpers.ts
+++ app/api/helpers.ts
@@ -1,5 +1,13 @@
 // check if email is valid
 export const isValidEmail = (email: string): boolean => {
     const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/
+
     return regex.test(email)
+}
+
+// remove special characters
+export const removeSpecialChar = (data: string) => {
+    const regex = /[^a-zA-Z0-9]/g
+
+    return data.replace(regex, '')
 }
