c19cd8f20771d90f160586a50b7c3473e43393d3
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-12 11:55:58 -0300

    feat: added routes

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -8,14 +8,16 @@ import { GrClose } from "react-icons/gr";
 import { AiTwotoneCloseCircle } from "react-icons/ai";
 import Button from "../button/Button";
 import { useState } from "react";
-import { clsx } from "clsx"
-import useStep from "@/hooks/useStep";
+import { clsx } from "clsx";
+import { usePathname, useRouter } from "next/navigation";
 
 
 // bootom bar
 const Navbar = () => {
-    // step setter hook
-    const { setStep, step } = useStep()
+    // get router
+    const router = useRouter()
+    // get pathname 
+    const pathname = usePathname()
     // deal with mobile menu show/hide menu options
     const [isMobileOpened, setIsMobileOpened] = useState<boolean>(false)
 
@@ -25,32 +27,32 @@ const Navbar = () => {
             <div className="hidden md:grid grid-cols-4 gap-4 h-[10%] w-[90%] rounded-md my-[1%]
             from-[#f1e499] via-[#b1ba27] to-[#888c08] bg-gradient-to-b drop-shadow-[0_1.4px_0.05rem] 
             shadow-[#00000092] border-[#b1ba27] border-[1px] min-h-fit overflow-hidden">
-                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => setStep(0)}>
+                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => router.push('/')}>
                     <PiChatTeardropFill size={20} />
                     Chats
                     {
-                        step === 0 && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
+                        pathname === '/' && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
                     }
                 </Button>
-                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => setStep(1)}>
+                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => router.push('/groups')}>
                     <PiChatsTeardropFill size={20} />
                     Groups
                     {
-                        step === 1 && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
+                        pathname === '/groups' && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
                     }
                 </Button>
-                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => setStep(2)}>
+                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => router.push('/settings')}>
                     <RiSettings2Fill size={20} />
                     Settings
                     {
-                        step === 2 && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
+                        pathname === '/settings' && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
                     }
                 </Button>
-                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => setStep(3)}>
+                <Button className="relative justify-center hover:bg-[#00000024]" onClick={() => router.push('/about')}>
                     <TbInfoSquareRoundedFilled size={20} />
                     About
                     {
-                        step === 3 && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
+                        pathname === '/about' && <AiTwotoneCloseCircle size={8} className="absolute top-auto bottom-auto right-2" />
                     }
                 </Button>
             </div>
@@ -71,28 +73,28 @@ const Navbar = () => {
                 {
                     isMobileOpened && (
                         <>
-                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => setStep(0)}>
+                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => router.push('/')}>
                                 <PiChatTeardropFill size={30} />
                                 {
-                                    step === 0 && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
+                                    pathname === '/' && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
                                 }
                             </Button>
-                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => setStep(1)}>
+                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => router.push('/groups')}>
                                 <PiChatsTeardropFill size={30} />
                                 {
-                                    step === 1 && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
+                                    pathname === '/groups' && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
                                 }
                             </Button>
-                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => setStep(2)}>
+                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => router.push('/settings')}>
                                 <RiSettings2Fill size={30} />
                                 {
-                                    step === 2 && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
+                                    pathname === '/settings' && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
                                 }
                             </Button>
-                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => setStep(3)}>
+                            <Button className="relative justify-center focus:bg-[#00000024]" onClick={() => router.push('/about')}>
                                 <TbInfoSquareRoundedFilled size={30} />
                                 {
-                                    step === 3 && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
+                                    pathname === '/about' && <AiTwotoneCloseCircle size={8} className="absolute top-1 right-1" />
                                 }
                             </Button>
                         </>
