e72de9e94d0579c5c394edf052d818aaad1fb82f
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-12 14:25:38 -0300

    feat: improved code

--- components/input/Input.tsx
+++ components/input/Input.tsx
@@ -30,13 +30,15 @@ const Input: React.FC<InputProps> = ({
             <label id={labelId} className="flex justify-start items-center py-2 px-4 h-fit w-full">
                 {label}
             </label>
-            <div className="relative flex justify-start items-center h-fit w-full">
+            <div className="relative flex justify-start items-center h-fit w-full bg-white
+            rounded-md overflow-hidden">
                 <input className={`flex justify-start items-center p-2 rounded-md h-fit w-full border-[1px]
-                ${errors[id] ? (
-                    'border-red-600'
-                ) : (
-                    'border-black'
-                )} ${isPassword && 'pr-16'}`}
+                    ${errors[id] ? (
+                        'border-red-600'
+                    ) : (
+                        'border-black'
+                    )} ${isPassword && 'pr-10 md:pr-16'}`}
+                    accept="image/png, image/jpeg"
                     placeholder={placeholder}
                     disabled={disabled}
                     required={required}
