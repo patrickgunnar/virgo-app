0802590b9420721b587fee6a81d97b91e4c49f99
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-12 11:21:54 -0300

    feat: added loading state

--- components/table/ClientOnly.tsx
+++ components/table/ClientOnly.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import ClientTable from "./ClientTable";
 import CredentialsTable from "./CredentialsTable";
 import { useSession } from "@/providers/SessionProvider";
@@ -15,29 +15,21 @@ const ClientOnly: React.FC<ClientOnlyProps> = ({
     children
 }) => {
     // get session
-    const { session } = useSession()
+    const { session, loading } = useSession()
     // use state to deal with on client state
     const [client, setClient] = useState<boolean>(false)
 
     // deal with the client only state
     // set client to true when on client side
     useEffect(() => setClient(true), [])
+    console.log(loading)
+
+    // current layout
+    const currentLayout = !session ? <CredentialsTable /> : <ClientTable>{children}</ClientTable>
 
     // render elements
     return (
-        client ? (
-            session ? (
-                <ClientTable>
-                    {children}
-                </ClientTable>
-            ) : (
-                <CredentialsTable />
-            )
-        ) : (
-            <div>
-                Loading...
-            </div>
-        )
+        client && loading === false ? currentLayout : <div>Loading</div>
     );
 }
  
