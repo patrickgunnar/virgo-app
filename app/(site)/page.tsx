d669a21ef1d64f024d104e40057a16ffaa506985
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-12 11:57:17 -0300

    refactor: chats page only

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,6 @@
-import PageContent from "./components/PageContent";
-
-
 export default function Home() {
     // render content
-    return <PageContent />
+    return (
+        <div>Chats</div>
+    )
 }
