85644ca2750e118f1af4307d92567794ea49e90f
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-11 21:02:39 -0300

    refactor: improved registration

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -4,57 +4,76 @@ import bcrypt from "bcrypt";
 import prisma from "@/components/libs/prismadb";
 import { isValidEmail, removeSpecialChar } from "../helpers";
 
-
-export async function POST(request: Request) {
+async function createUser(data: {
+    name: string;
+    email: string;
+    password: string;
+    username: string;
+}): Promise<{ token: string; data: any } | null> {
     try {
-        // get the body
-        const body = await request.json()
-        // get user data
-        const { email, password, name, username } = body
+        const { name, email, password, username } = data;
 
-        // if not expected data
-        if(!email || !password || !name || !username) throw new Error('Invalid data!')
+        if (!email || !password || !name || !username) {
+            throw new Error("Invalid data!");
+        }
 
-        // check email validity
-        const isEmail = isValidEmail(email)
-        
-        // if not a valid e-mail
-        if(!isEmail) throw new Error('Invalid data!')
+        const existingUserWithUsername = await prisma.user.findFirst({ where: { username } })
+        if(existingUserWithUsername) {
+            throw new Error('Username on use!')
+        }
 
-        // crypting user password
-        const hashedPassword = await bcrypt.hash(password, 12)
+        const isEmailValid = isValidEmail(email);
+        if (!isEmailValid) {
+            throw new Error("Invalid email!");
+        }
 
-        // if not hashed password
-        if(!hashedPassword) throw new Error('Something went wrong!')
+        const existingUserWithEmail = await prisma.user.findFirst({ where: { email } })
+        if (existingUserWithEmail) {
+            throw new Error("E-mail on use!");
+        }
 
-        // creating an user
-        const user = await prisma.users.create({
+        const hashedPassword = await bcrypt.hash(password, 12);
+        if (!hashedPassword) {
+            throw new Error("Something went wrong!");
+        }
+
+        const user = await prisma.user.create({
             data: {
                 name,
                 email,
                 username: removeSpecialChar(username),
                 hashedPassword,
-                image: '',
-                groupId: [],
-                bio: 'Welcome to Virgo Chat'
-            }
-        })
-        console.log(user)
+                image: "",
+                bio: "Welcome to Virgo Chat",
+            },
+        });
 
-        // jwt secret key
-        const secretKey = process.env.JWT_SECRET || ''
-        // Generate a JWT token with the user's data
-        const token = jwt.sign({
-            ...user
-        }, secretKey, { expiresIn: '1w' })
+        const secretKey = process.env.JWT_SECRET || "";
+        const token = jwt.sign({ ...user }, secretKey, { expiresIn: "1w" });
 
-        return NextResponse.json({
-            token: token,
-            data: user,
-        })
+        return { token, data: user };
     } catch (error: any) {
-        return new NextResponse('Internal error!', {
+        console.error("User registration error:", error);
+        return null;
+    }
+}
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json();
+        const user = await createUser(body);
+
+        if (!user) {
+            return new NextResponse("Internal error!", {
+                status: 500,
+            });
+        }
+
+        return NextResponse.json(user);
+    } catch (error: any) {
+        console.error("POST request error:", error);
+        return new NextResponse("Internal error!", {
             status: 500
-        })
+        });
     }
 }
