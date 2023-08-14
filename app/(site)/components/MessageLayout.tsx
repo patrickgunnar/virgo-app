3cea6736b2d9e851711b2a02a6fc1b8b94f2e191
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-14 14:09:35 -0300

    feat: added responsivity

--- app/(site)/components/MessageLayout.tsx
+++ app/(site)/components/MessageLayout.tsx
@@ -19,12 +19,12 @@ const MessageLayout: React.FC<MessageLayoutProps> = ({
     name, username, image, message, isCurrentUser
 }) => {
     return (
-        <div className={clsx(`flex gap-4 justify-start items-center py-2 px-4 my-2 h-fit w-fit max-w-[70%] rounded-md 
+        <div className={clsx(`flex gap-4 justify-start items-center py-2 px-4 my-2 h-fit w-full md:w-fit md:max-w-[70%] rounded-md 
         from-[#ff9e1b] to-[#ffcf54] bg-gradient-to-b border-[#ff9e1b] border-[1px] drop-shadow-[0_0_0.1rem] 
         shadow-[rgba(0,0,0,0.57)]`, isCurrentUser ? 'self-end' : 'self-start')}>
             {
                 image ? (
-                    <div className="relative flex h-16 aspect-square rounded-full drop-shadow-[0_0_0.05rem] 
+                    <div className="relative hidden md:flex h-16 aspect-square rounded-full drop-shadow-[0_0_0.05rem] 
                         shadow-[#00000080] border-[#481811] border-[1px] overflow-hidden ml-2">
                         <Image className="object-cover"
                             src={image}
@@ -33,13 +33,14 @@ const MessageLayout: React.FC<MessageLayoutProps> = ({
                         />
                     </div>
                 ) : (
-                    <div className="relative flex h-16 aspect-square overflow-hidden ml-2">
+                    <div className="relative hidden md:flex h-16 aspect-square overflow-hidden ml-2">
                         <IoPersonCircleSharp className="h-full w-full" />
                     </div>
                 )
             }
-            <div className="flex flex-col flex-wrap justify-center items-start text-left h-fit w-fit  max-w-[85%]">
-                <div className="relative truncate text-lg text-black font-bold h-fit w-fit">
+            <div className="relative flex flex-col flex-wrap justify-center items-start text-left h-fit w-full md:w-fit 
+            md:max-w-[75%] xl:max-w-[85%]">
+                <div className="relative break-words text-lg text-black font-bold h-fit w-fit">
                     {name}
                     <span className="inline-block mx-2 my-[2px]">
                         <BsCircleFill size={5} />
