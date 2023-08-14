e909388a3d12cbb4bed73113314a8ec0261c301e
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-14 09:13:01 -0300

    feat: loading layout

---
+++ components/button/loading/Loading.tsx
@@ -0,0 +1,12 @@
+'use client'
+
+import styles from "./styles.module.css";
+
+
+const Loading = () => {
+    return (
+        <span className={styles.button_loader}></span>
+    );
+}
+ 
+export default Loading;
