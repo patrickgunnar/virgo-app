a4b80cfe9d7c04b3a7f74958b012d05e7e696dc1
Patrick Gunnar <patrickgunnar.me@gmail.com>
2023-08-14 10:40:10 -0300

    feat: get user details

---
+++ app/api/get-user/route.ts
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import prisma from "@/components/libs/prismadb";
+
+
+export async function POST(request: Request) {
+    try {
+        const body = await request.json()
+        const { userId } = body
+
+        if (!userId || typeof userId !== "string") {
+            throw new Error("Invalid username format.")
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        })
+
+        if(!user) throw new Error('User not found!')
+
+        return NextResponse.json({
+            username: user.username,
+            name: user.name
+        })
+    } catch (error: any) {
+        console.error("Error in POST request:", error)
+
+        return new NextResponse("Internal error!", {
+            status: 500,
+        })
+    }
+}
